7d83125bdd7bb96a7ef085bfc667023ea716142d
Juan Tenaglia <juantenaglia81296@gmail.com>
2022-04-12 15:00:01 -0300

    desafio2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from"./components/NavBar";
+import DaisyNavBar from "./components/DaisyNavBar";
 
 function App() {
+  const styles ={
+    title:"text-3xl font-bold font-mono text-blue-400"
+  }
   return (
     <div className="App">
-     <h1>¡Hola mundo con React!</h1>
+      <DaisyNavBar/>
+      <NavBar/>
+      <h1 className={styles.title}>¡Hola mundo con React!</h1>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+
+    const estilos = {
+        border: "solid red 1px",
+        color: "blue"
+    }
+  return (
+    <div>
+        <li  style ={estilos}>Inicio</li>
+        <li style ={{color:"red"}}>Computadoras</li>
+        <li>Celulares</li>
+        <li>Tablets</li>
+    </div>
+  )
+}
+export default NavBar
