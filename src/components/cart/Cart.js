f3ec7ebade63547afc9cdebe3cecadb94ffc9d42
Juan Tenaglia <juantenaglia81296@gmail.com>
2022-05-14 14:57:01 -0300

    agregandoCarrito5

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,27 +3,19 @@ import CartListCard from "./CartListCard"
 
 
   const CartList = ({ producto }) => {
-    const { deleteFromCart,deleteCart,cart } = useCartContext(CartContextProvider);
+    const {deleteCart,cart } = useCartContext(CartContextProvider);
   
-    const handleRemove = producto => {
-      deleteFromCart(producto);
-    }
 
   
   return (
     <>
-      <div style={{fontSize:"30px"}}>Carrito</div>
+      <div style={{fontSize:"50px",fontWeight:"bolder"}}>Tu compra</div>
       <button
         className="btn btn-success"
         onClick={ () => { deleteCart() } }>
           Vaciar lista
       </button>
       { cart.map( f => <CartListCard key={f.id} product={f}/> ) }
-       <button
-       className="btn btn-success"
-       onClick={ () => { handleRemove(producto) } }>
-         Eliminar Item
-     </button>
      </>
   )
 }
--- src/components/cart/CartListCard.js
+++ src/components/cart/CartListCard.js
@@ -1,12 +1,26 @@
+import CartContextProvider, { useCartContext } from "../context/CartContext";
+
 const CartListCard = ( { product } ) => {
+
+  const { deleteFromCart } = useCartContext(CartContextProvider);
+  
+    const handleRemove = product => {
+      deleteFromCart(product);
+    }
+
   
     return (
       <div style={{fontWeight:"bolder",paddingTop:"50px"}}>
         	<figure>
-					<img style={{height:"400px",width:"300px"}} src={product.picUrl} alt="Product" />
+					<img src={product.picUrl} alt="Product" />
 				</figure>
          <strong style={{fontSize:"40px"}}>{product.title}</strong>
           <h1 style={{fontWeight:"boder",fontSize:"30px",color:"darkBlue"}}> Cantidad de este producto: {product.quantity} </h1>
+          <button
+       className="btn btn-success"
+       onClick={ () => { handleRemove(product) } }>
+         Eliminar Item
+     </button>
       </div>
     )
   }
